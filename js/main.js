a1978c8368f113b12892d778126e4942b688a08e
noah <ehson.ehsonov@gmail.com>
2024-04-01 22:12:37 +0300

    Изменил loader и добавил слайдшоу

--- js/main.js
+++ js/main.js
@@ -7,5 +7,29 @@ window.addEventListener('load', () => {
     }, 800)
 });
 
+
+let indexValue = 0;
+function slideShow() {
+    setTimeout(slideShow, 2000)
+    let x;
+    const imageComparison = document.querySelectorAll('.comparison');
+    for (let x = 0; x < imageComparison.length; x++) {
+        imageComparison[x].style.display = 'none';
+    }
+    indexValue++;
+    if (indexValue > imageComparison.length) {
+        indexValue = 1;
+    }
+    imageComparison[indexValue - 1].style.display = 'block';
+}
+slideShow();
+
+
+
+
+
+
+
+
 AOS.init();
 
